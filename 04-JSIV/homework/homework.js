0b6f8c80d00d197d1aae8707ce55500512b2a2ea
Erica <erica.rivarola@gmail.com>
2021-11-05 11:39:24 -0300

    Agregando nuevo archivo, 04-JSIV/homework/homework.js

--- 04-JSIV/homework/homework.js
+++ 04-JSIV/homework/homework.js
@@ -3,14 +3,15 @@
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   // Tu código:
-
+cb ()
 }
+
 
 function operacionMatematica(n1, n2, cb) {
   // Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   // Devolver lo que retorne el ejecutar el callback pasándole como argumentos los números recibidos.
   // Tu código:
-
+return cb (n1,n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -18,29 +19,42 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   // Tu código:
-
+  var suma = 0;
+for (var i = 0; i < numeros.length; i++) {
+ suma = suma + numeros [i];
+  
+}
+cb (suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre el array "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada elemento el arreglo)
   // Tu código:
-
+array.forEach(cb);
 }
 
 function map(array, cb) {
   // Itera sobre cada elemento de "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   // Tu código:
-
+  var nuevoArray = [];
+for (var i = 0; i < array.length; i++) {
+ nuevoArray.push (cb(array[i]));
+   
 }
+return nuevoArray;
+}
+
 
 function filter(array) {
   // Filtrar todos los elementos del array que comiencen con la letra "a".
   // Devolver un nuevo array con los elementos que cumplen la condición
   // Tu código:
-
+var filtrados = array.filter(elemento => elemento[0] === "a");
+return filtrados;
 }
+
 
 // ---- Ejercicios de Repaso ----
 
@@ -54,30 +68,57 @@ function deObjetoArray(objeto){
       C: 3
      }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   // Tu código:
-
+return Object.entries(objeto);
 }
+
+
 
 function numberOfCharacters(string) {
   // La función recibe un string. Recorre el srting y devuelve el caracter con el número de veces que aparece 
   // en formato par clave-valor.
   // Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   // Tu código:
-
+  var repe = {};
+  for (let i = 0; i < string.length; i++) {
+    if(!repe[string[i]]) {
+      repe[string[i]] = 1;
+    }
+      else {
+        repe[string[i]] = repe[string[i]] + 1; 
+      }
+    }
+  return repe;
+  
+  
 }
+
 
 function capicua(numero){
   // Escribe una función, la cual recibe un número y determina si es o no capicúa.
   // La misma debe retornar: "Es capicua" si el número se lee igual de 
   // izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   // Tu código:
-
+var numeroStr= numero.toString();
+var numeroStrInvertido=  numeroStr.split('').reverse().join('');
+if (numeroStr === numeroStrInvertido) return "Es capicua";
+return "No es capicua";
+  
+  
 }
+  
 
 function deleteAbc(cadena){
   // Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   // y devuelva la versión modificada o la misma cadena, en caso de no contener dichas letras.
   // Tu código:
-
+  var cadenaNueva= "";
+  for (var i = 0; i < cadena.length; i++) {
+    if(cadena[i] !== "a" && cadena[i] !== "b" && cadena[i] !== "c"){
+      cadenaNueva = cadenaNueva + cadena[i];
+    }
+    
+  }
+return cadenaNueva;
 }
 
 function buscoInterseccion(arreglo1, arreglo2){
@@ -86,7 +127,13 @@ function buscoInterseccion(arreglo1, arreglo2){
   // Si no tienen elementos en común, retornar un arreglo vacío.
   // Aclaración: los arreglos no necesariamente tienen la misma longitud
   // Tu código:
-
+  var interseccion = [];
+  for (var i = 0; i < arreglo1.length; i++) {
+    for (var j = 0; j < arreglo2.length; j++) {
+      if(arreglo1[i] === arreglo2[j]) interseccion.push(arreglo1[i]);
+    }
+  }
+  return interseccion;
 }
 
 // No modificar nada debajo de esta línea
